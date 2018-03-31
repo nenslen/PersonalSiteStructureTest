978a6b628533930cb1ae6233b3dbcc4c32301af6
nenslen <nicolas.enslen@gmail.com>
2018-03-30 21:15:07 -0600

    Changed section headers, fixed game width issue

--- projects/demos/ultimate-tic-tac-toe/index.php
+++ projects/demos/ultimate-tic-tac-toe/index.php
@@ -10,16 +10,6 @@
 	<script src="js/tictactoe.js"></script>
 	<script src="js/gamelogic.js"></script>
 	<script src="js/bot.js"></script>
-	<!--
-	<title>Ultimate Tic Tac Toe</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css'>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script src="TicTacToe.js"></script>
-	<script src="gameLogic.js"></script>
-	<script src="Bot.js"></script>
--->
 </head>
 <body>
 	<?php drawHeader('small'); ?>
@@ -35,18 +25,19 @@
 						<?php drawBoard(); ?>
 					</div>
 
+					<div class="grid">
+						<button class="button" id="startButton">New Game</button>
+					</div>
 
-
-					<button class="button" id="startButton">New Game</button>
 					<div class="card" id="settings-menu">
-
+						<!--
 						<span class="item">
 							Game Speed
 							<div class="gameSpeed_slider">
 								<input class="gameSpeed_range" type="range" value="40" min="1" max="200">
 							</div>
 						</span>
-						
+						-->
 
 						<div class="grid grid-2">
 							<!-- Difficulty -->
@@ -87,14 +78,12 @@
 					</div>
 					
 
-			        <h1 class="section-header">About</h1>
-
+			        <h1 class="section-header top-header">About</h1>
 			        <p id="description">About ultimate tic tac toe</p>
 			        
 
-			        <h1 class="section-header">Details</h1>
-
-			        <h2 class="section-header-small">How it works</h2>
+			        <h1 class="section-header top-header">Details</h1>
+			        <h2 class="section-header section-header-small">How it works</h2>
 			        <p>How it works section</p>
 			        
 			        <p>For source code, visit the <a href="https://github.com/nenslen/Connect4">github page for this project</a>.</p>
